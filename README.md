65e6fd4f4a226cc7e9f25a4fecc8a440eb1587ed
geekpradd <geekpradd@gmail.com>
2020-04-24 02:02:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## CovEd AutoMailing
+
+This repository contains the code for auto informing and mailing mentors and mentees of their matchings.
+
+### Usage
+
+After installing the google OAuth python library run both the main files. You will have to login into the main google account via the browswer. This will generate pickle files that will store this logged in state. It is imperative that this pickle file is not shared as doing so would cause breach of security. Appropriate gitignores will be used.
+
---
+++ main.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+import base64
+
+from sheets import get_sheet_data
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://mail.google.com/']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    send_matching_mails(service)    
+
+
+
+def create_message(sender, to, subject, message_text):
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  raw_message = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
+  return {
+    'raw': raw_message.decode("utf-8")
+  }
+
+
+def create_draft(service, user_id, message_body):
+  try:
+    message = {'message': message_body}
+    draft = service.users().drafts().create(userId=user_id, body=message).execute()
+
+    print("Draft id: %s\nDraft message: %s" % (draft['id'], draft['message']))
+
+    return draft
+  except Exception as e:
+    print('An error occurred: %s' % e)
+    return None 
+
+def send_message(service, user_id, message):
+  try:
+    message = service.users().messages().send(userId=user_id, body=message).execute()
+    print('Message Id: %s' % message['id'])
+    return message
+  except Exception as e:
+    print('An error occurred: %s' % e)
+    return None
+
+def send_matching_mails(service):
+    mentor_emails, data = get_sheet_data()
+
+    for email in mentor_emails:
+        message = create_message("covedindia@gmail.com", email, "Coved India: Mentee has been assigned", format_body(data))
+        send_message(service, 'me', message)
+
+if __name__ == '__main__':
+    main()
